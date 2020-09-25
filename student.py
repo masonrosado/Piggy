b52f135208d813a110b9afa5df70cf1eb0c02df7
masonrosado <71452027+masonrosado@users.noreply.github.com>
2020-09-25 11:19:22 -0400

    spin update typo

--- student.py
+++ student.py
@@ -60,7 +60,7 @@ class Piggy(PiggyParent):
         """A higher-ordered algorithm to make your robot dance"""
         # TODO: check to see if it's safe before dancing
         
-        self.spindizzy()
+        self.spin_dizzy()
         self.for_back()
         #self.break_neck()
         
