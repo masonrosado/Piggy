0d2326aacc133543167aef23e49309a43c44942f
masonrosado <71452027+masonrosado@users.noreply.github.com>
2020-10-07 11:15:29 -0400

    cleaning up nav and adding until clear

--- student.py
+++ student.py
@@ -198,6 +198,16 @@ class Piggy(PiggyParent):
         #if the three-part check didn't freak out
         return True
 
+    def turn_until_clear(self):
+        """ Rotate left until no obstacle is seen"""
+        print("Rotating until lane is clear")
+        #make sure robot is looking straigh
+        self.servo(self.MIDPOINT)
+        while self.read_distance() < self.SAFE_DISTANCE:
+            self.left(primary=40,counter=-40)
+            time.sleep(.05)
+        #stop motion before ending method
+        self.stop()
 
     def nav(self):
         """ Top Gun Initiative """
@@ -205,17 +215,14 @@ class Piggy(PiggyParent):
         print("-------- [ Press CTRL + C to stop me ] --------\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         
-        # TODO: build self.quick_check() that does a fast, 3-part check instead of read_distance
-        self.fwd()
+
         while True:
             if not self.quick_check():
                 self.stop()
-                print("There's a body in the road...") 
-                self.turn_by_deg(90)
-                time.sleep(.1)
+                self.turn_until_clear()
             else:
                 self.fwd()
-        self.stop()
+                
         # TODO: scan so we can decide left or right
         # TODO: average the right side of the scan dict
         # TODO: average the left side of the scan dict
